bb5f6820a418c9203f40a228b02c93ff8d6f2f95
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-07-08 22:30:14 +0900

    additional libraries

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     presets: [
-        ["@babel/preset-env", {targets: "> 1%, not dead"}]
+        ["@babel/preset-env", { targets: "> 1%, not dead" }]
     ],
-    plugins: ["@babel/plugin-syntax-dynamic-import"]
+    plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties"
+    ]
 }
